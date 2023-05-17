5570f81ef8bb49fa9e44e7223eedae5fd6506d6d
Alan Dearle <al@st-andrews.ac.uk>
2023-05-17 15:55:03 +0100

    Started to write test harnesses

---
+++ simcoder/perfect_point_harness.py
@@ -0,0 +1,11 @@
+# Runs the perfect point experiment
+# Assumes data is in /Volumes/Data
+# 
+# Returns a Pandas dataframe containing the  category_number, category_label, recall_at_k
+
+import numpy as np
+import pandas as pd
+
+def run_experiment_perfect_point(self, embeddings : np.array, categorised_embeddings : np.array, number_experiments : int, category_threshold : float, k : int ) -> pd.DataFrame:
+    pass
+
