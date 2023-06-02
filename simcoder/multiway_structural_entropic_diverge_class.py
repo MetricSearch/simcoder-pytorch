6b65621bd2bf6e5d7ee5b5d1648395a1301e952a
David <dm236@st-andrews.ac.uk>
2023-06-02 13:38:51 +0100

    Improve DMs MSED implementation.

--- simcoder/multiway_structural_entropic_diverge_class.py
+++ simcoder/multiway_structural_entropic_diverge_class.py
@@ -14,48 +14,73 @@ def complexity(X: np.array) -> np.array:
     """
     log_X = np.log(X)
     X_log_X = X * log_X
-    sum_X_log_X = np.sum(X_log_X, axis=1, keepdims=True)
+    sum_X_log_X = np.nansum(X_log_X, axis=1, keepdims=True)
     res = np.exp(-sum_X_log_X)
     return res
 
 def nth_root(a: np.array, n: int):
+    """Computes the nth root of each element in the input array.
+
+    Args:
+        a (np.array): Input array.
+        n (int): The root value.
+
+    Returns:
+        np.array: Resultant array with nth root of each element.
+    """
     return np.power(a,(1/n))
 
 class MSED:
+    """Computes the multiway structural entropic divergence of a set of
+    probability vectors where the sum of a vectors components equals 1.
+    """
     def __init__(self, base: np.array) -> None:
+        """Initialize the MSED instance.
+
+        Args:
+            base (np.array): The base vectors, a 2d array where each row represents a probability vector.
+        """
         assert np.all(np.isclose(np.sum(base, axis=1), 1.0)), "All row vectors in the base vectors must sum to 1."
         assert len(base.shape) == 2, "The base vectors must be a 2d array." 
 
-        self.num_base_vectors = base.shape[0]
-        self.sums_of_base_vectors = np.sum(base, axis=0, keepdims=True)
-        self.product_of_base_vector_complexities = np.product(complexity(base))
+        self.num_vecs = base.shape[0]
+        self.sums_of_vecs = np.sum(base, axis=0, keepdims=True)
+        self.prod_of_vec_comp = np.product(complexity(base))
 
     def __call__(self, queries: np.array) -> np.array:
-        num_vectors = self.num_base_vectors + 1
+        """Compute the multiway structural entropic divergence for the given queries.
+
+        Args:
+            queries (np.array): The query vectors, a 2d array where each row represents a probability vector.
+
+        Returns:
+            np.array: The computed multiway structural entropic divergence for each query.
+        """
+        num_vecs = self.num_vecs + 1
         
         # compute the numerator
         # a scalar - the complexity of the average of the vectors for base + each query
-        average_vector_for_each_query = (queries + self.sums_of_base_vectors) / num_vectors
-        complexity_of_average_vector_for_each_query = complexity(average_vector_for_each_query)
+        avg_vec = (queries + self.sums_of_vecs) / num_vecs
+        comp_of_avg_vec = complexity(avg_vec)
 
         # compute the denominator
         # a scalar - nth root of the product of the vector complexities for base + each query
-        complexity_of_queries = complexity(queries)
-        product_of_vector_complexities_for_each_query = self.product_of_base_vector_complexities * complexity_of_queries
-        nth_root_of_product_of_vector_complexities_for_each_query = nth_root(product_of_vector_complexities_for_each_query, num_vectors)
+        comp_of_queries = complexity(queries)
+        prod_of_vec_comp = self.prod_of_vec_comp * comp_of_queries
+        nth_root_of_prod_of_vec_comp = nth_root(prod_of_vec_comp, num_vecs)
 
         # compute the unscaled result 1.0-n
-        res = complexity_of_average_vector_for_each_query / nth_root_of_product_of_vector_complexities_for_each_query
-        assert np.all(res >= 1), f"Ratio should be greater than or equal to one. Array is {res}."
-        assert np.all(res <= num_vectors), f"Ratio must be less than or equal to the number of vectors. Array is {res}."
+        res = comp_of_avg_vec / nth_root_of_prod_of_vec_comp
+        assert np.all(res >= 1), f"Ratio should be greater than 1."
+        assert np.all(res <= num_vecs), f"Ratio must be less than or equal to number of vectors."
 
         # scale the result to 0.0-1.0
-        res = (res - 1) / (num_vectors - 1)
+        res = (res - 1) / (num_vecs - 1)
         return res
     
 if __name__ == '__main__':
-    base_matrix = np.array([[0.2, 0.6, 0.2],[0.1, 0.7, 0.2],[0.2, 0.5, 0.3]])
+    base = np.array([[0.2, 0.6, 0.2],[0.1, 0.7, 0.2],[0.2, 0.5, 0.3]])
     data = np.array([[0.2, 0.5, 0.3],[0.2, 0.7, 0.1]])
-    msed = MSED(base_matrix)
+    msed = MSED(base)
     res = msed(data)
     print(res)
