1b03fe622c1a454d68dbcb407432caf8fa0cb700
David <dm236@st-andrews.ac.uk>
2023-06-12 13:45:40 +0100

    Refactor - Rename getDists to get_dists

--- sisap2023/utils/similarity.py
+++ sisap2023/utils/similarity.py
@@ -18,37 +18,22 @@ def get_mf_image(index: int) -> Image.Image:
     img = default_loader(str(path))
     return img
 
+def make_mf_image_grid(
+    img_indices: np.array, num_cols: int, num_rows: int, img_w: int, img_h: int
+) -> Image.Image:
+    images = [get_mf_image(i) for i in img_indices]
+    grid = Image.new("RGB", size=(num_cols * img_w, num_rows * img_h))
+    for idx, img in enumerate(images):
+        img = img.resize((img_w, img_h), Image.Resampling.BILINEAR)
+        grid.paste(img, box=(idx % num_cols * img_w, idx // num_cols * img_h))
+    return grid
+
 def load_encodings(encodings_dir: Path, key: str = 'features') -> np.array:
     paths = encodings_dir.glob("*.mat")
     paths = sorted(paths, key=lambda p: int(p.stem))
     encodings = [loadmat(p)[key] for p in paths]
     encodings = np.concatenate(encodings)
     return encodings
-
-# def load_mf_encodings(encodings_dir: Path) -> np.array:
-#     return load_encodings_mat(encodings_dir, key='features')
-
-# def load_mf_softmax(encodings_dir: Path) -> np.array:
-#    return load_encodings_mat(encodings_dir, key='probVecs')
-
-
-def encode(query_image: Image.Image, model_name: str) -> np.array:
-    # setup the pytorch device
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-    # get the model
-    model, preprocess = get_model(model_name)
-    model.to(device)
-
-    # preprocess the image into a tensor, 
-    # add a fake batch axis
-    # send it to the device (gpu or cpu)
-    img = preprocess(query_image).unsqueeze(0).to(device)
-
-    with torch.no_grad():
-        features = model(img)
-        features = features.detach().cpu().numpy()
-    return features
 
 def l1_norm(X):
     row_sums = np.sum(X,axis=1)
@@ -69,37 +54,9 @@ def euclid(img_features: np.array, encodings: np.array):
     distances = np.sqrt(np.sum(np.square((img_features - encodings)), axis=1))
     return distances
 
-def getDists(query_index,allData):
+def get_dists(query_index,allData):
     '''Return the distances from the query to allData'''
     '''Returns an array same dimension as allData of scalars'''
     mf_query_data = allData[query_index]
     distances = euclid(mf_query_data, allData)
     return distances
-
-def make_mf_image_grid(
-    img_indices: np.array, num_cols: int, num_rows: int, img_w: int, img_h: int
-) -> Image.Image:
-    images = [get_mf_image(i) for i in img_indices]
-    grid = Image.new("RGB", size=(num_cols * img_w, num_rows * img_h))
-    for idx, img in enumerate(images):
-        img = img.resize((img_w, img_h), Image.Resampling.BILINEAR)
-        grid.paste(img, box=(idx % num_cols * img_w, idx // num_cols * img_h))
-    return grid
-
-
-def show_features(features: np.array):
-    plt.plot(features)
-    plt.show()
-
-
-def get_similar_mf(
-    query_image: Image.Image, encoder_name: str, img_size: int, 
-) -> Image.Image:
-    features = encode(query_image, encoder_name)
-    mf_encodings = load_mf_encodings(Path("/output", f"mf_{encoder_name}"))
-    distances = euclid(features, mf_encodings)
-    print(np.sort(distances)[:100])
-    sorted_indices = np.argsort(distances)
-    top_ten_indices = sorted_indices[:100]
-    print(top_ten_indices)
-    return make_mf_image_grid(top_ten_indices, 10, 10, img_size, img_size)
