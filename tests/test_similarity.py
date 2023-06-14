2485c184820f0a97061b72fb579aee6ae9e18e83
David <dm236@st-andrews.ac.uk>
2023-06-14 20:36:15 +0100

    Remove unused tests package

--- tests/test_similarity.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from sisap2023.utils.mirflickr import *
-
-class TestSimilarity(unittest.TestCase):
-    def test_encode(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
