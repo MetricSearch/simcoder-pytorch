b71a1630095cab5ebfd508ce03d85e355909c0d9
David <dm236@st-andrews.ac.uk>
2023-06-15 12:43:31 +0100

    Update best_queries notebook

--- sisap2023/utils/mirflickr.py
+++ sisap2023/utils/mirflickr.py
@@ -10,9 +10,15 @@ import torch
 from sisap2023.encoders.models import get_model
 
 IMAGE_FILES_PER_FOLDER = 10000
-mf_dir = Path("/Volumes/Data/mf/images/")  # <<<<<<<<<<<<<<<<<<<<<<<<<< TODO fix me
+mf_dir = None
+
+def set_mf_images_path(path: Path) -> None:
+    global mf_dir
+    assert path.exists(), "Path to mf images does not exist."
+    mf_dir = path
 
 def get_mf_image(index: int) -> Image.Image:
+    assert mf_dir, "mf_dir is not set. Please set it to a Path to the mf images."
     folder_idx = index // IMAGE_FILES_PER_FOLDER
     path = mf_dir / str(folder_idx) / f"{index}.jpg"
     img = default_loader(str(path))
@@ -21,6 +27,7 @@ def get_mf_image(index: int) -> Image.Image:
 def make_mf_image_grid(
     img_indices: np.array, num_cols: int, num_rows: int, img_w: int, img_h: int
 ) -> Image.Image:
+    assert mf_dir, "mf_dir is not set. Please set it to a Path to the mf images."    
     images = [get_mf_image(i) for i in img_indices]
     grid = Image.new("RGB", size=(num_cols * img_w, num_rows * img_h))
     for idx, img in enumerate(images):
